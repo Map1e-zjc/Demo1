17b2d981ee93d8501b44ee922ba3323de877e859
pan11123 <13173820978@163.com>
2025-04-16 13:19:51 +0800

    AboutUs页面

--- pages/User/AboutUs.vue
+++ pages/User/AboutUs.vue
@@ -1,22 +1,125 @@
 <template>
-	<view>
+	<view class="container">
+		<view class="title">
+			招商咨询
+		</view>
 		
+		<view class="info-list">
+			<view class="info-item">
+				<view class="icon-box">
+					<image src="/static/other_icons/联系我们-选中.png" class="icon" mode="aspectFit"></image>
+				</view>
+				<view class="info-content">
+					<view class="label">招商热线</view>
+					<view class="value">0577-88123456</view>
+				</view>
+			</view>
+			
+			<view class="info-item">
+				<view class="icon-box">
+					<image src="/static/other_icons/人像.png" class="icon" mode="aspectFit"></image>
+				</view>
+				<view class="info-content">
+					<view class="label">联系人</view>
+					<view class="value">张经理</view>
+				</view>
+			</view>
+			
+			<view class="info-item">
+				<view class="icon-box">
+					<image src="/static/other_icons/电子邮箱.png" class="icon" mode="aspectFit"></image>
+				</view>
+				<view class="info-content">
+					<view class="label">电子邮箱</view>
+					<view class="value">zhaoshang@ouyu.com</view>
+				</view>
+			</view>
+			
+			<view class="info-item">
+				<view class="icon-box">
+					<image src="/static/other_icons/公司地址.png" class="icon" mode="aspectFit"></image>
+				</view>
+				<view class="info-content">
+					<view class="label">公司地址</view>
+					<view class="value">温州市瓯城区车站大道100号</view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="feedback-btn">
+			意见反馈
+		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+<script setup>
+	
 </script>
 
-<style>
+<style scoped>
+.container {
+	padding: 0 20rpx;
+}
 
+.title {
+	font-size: 36rpx;
+	font-weight: 600;
+	text-align: center;
+	padding: 30rpx 0;
+}
+
+.info-list {
+	border-radius: 10rpx;
+	overflow: hidden;
+	background-color: #FFFFFF;
+}
+
+.info-item {
+	display: flex;
+	padding: 30rpx 20rpx;
+	border-bottom: 1px solid #F5F5F5;
+}
+
+.icon-box {
+	width: 80rpx;
+	height: 80rpx;
+	background-color: #F0F7FF;
+	border-radius: 50%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 20rpx;
+}
+
+.icon {
+	width: 40rpx;
+	height: 40rpx;
+}
+
+.info-content {
+	flex: 1;
+}
+
+.label {
+	font-size: 28rpx;
+	color: #999;
+	margin-bottom: 10rpx;
+}
+
+.value {
+	font-size: 32rpx;
+	font-weight: 600;
+	color: #333;
+}
+
+.feedback-btn {
+	margin-top: 60rpx;
+	background-color: #FFFFFF;
+	text-align: center;
+	padding: 30rpx 0;
+	border-radius: 10rpx;
+	font-size: 32rpx;
+	color: #333;
+	border: 1px solid #E5E5E5;
+}
 </style>
--- pages/User/User.vue
+++ pages/User/User.vue
@@ -1,22 +1,63 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view class="goto" @click="goSomewhere1">
+			跳转1
+		</view>
+		<view class="goto" @click="goSomewhere2">
+			跳转2
+		</view>
+		<view class="about" @click="goAboutUs">
+			关于我们
+		</view>
 	</view>
+	
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
+export default {
+  data() {
+    return {
+		
 	}
+  },
+  methods: {
+	goSomewhere1() {
+		uni.navigateTo({
+			url: "/pages/User/AboutUs"
+		})
+	},
+	goSomewhere2() {
+		uni.navigateTo({
+			url: "/pages/User/AboutUs"
+		})
+	},
+	goAboutUs() {
+		uni.navigateTo({
+			url: "/pages/User/AboutUs"
+		})
+	}
+  }
+}
 </script>
 
-<style>
-
+<style scoped>
+.container {
+	padding: 0 20rpx;
+}	
+	
+.goto {
+	padding: 50rpx;
+	text-align: center;
+	font-size: 40rpx;
+	border: 1px solid #e5e5e5 ;
+	background-color: #eee;
+	margin: 10rpx 0;
+}
+.about {
+	padding: 50rpx;
+	text-align: center;
+	font-size: 40rpx;
+	border: 1px solid #e5e5e5 ;
+	background-color: #eee;
+}
 </style>
