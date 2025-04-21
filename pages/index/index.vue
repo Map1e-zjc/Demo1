8bdb60641751cbac0c4a19ecd6cf4f6c4cffc6d3
Map1e <1187904198@qq.com>
2025-04-21 09:19:11 +0800

    模拟项目初步完成

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,20 +1,27 @@
 <template>
   <view class="map-container">
-    <map 
-      id="wzMap"
-      :latitude="28.007675"
-      :longitude="120.655421"
-      :markers="markers"
-      :scale="13"
-      style="width: 100%; height: 100vh"
-      show-location
-      enable-traffic
-      @markertap="handleMarkerTap">
-    </map>
+	<LoadingComponent 
+		:isLoading="isLoading"
+		text="地图加载中"
+		color="#2979FF"
+		:size="40"
+	  />
+	<map 
+	  id="wzMap"
+	  :latitude="28.007675"
+	  :longitude="120.655421"
+	  :markers="markers"
+	  :scale="13"
+	  style="width: 100%; height: 100vh"
+	  show-location
+	  enable-traffic
+	  @markertap="handleMarkerTap">
+	</map>
   </view>
 </template>
 
 <script>
+import LoadingComponent from '@/components/LoadingComponent.vue'
 export default {
   data() {
     return {
@@ -23,66 +30,77 @@ export default {
         latitude: 28.007675,
         longitude: 120.655421
       },
-      
+      isLoading:true,
       // 标记点数据（瓯海区 & 鹿城区）
-      markers: [
-        {
-          id: 1,
-          title: "瓯海区政府",
-          latitude: 28.01332,
-          longitude: 120.66874,
-          width: 24,    // 必须参数
-          height: 34,   // 必须参数
-          callout: {
-            content: "瓯海区政府",  // 常显信息窗
-            color: "#FFFFFF",
-            bgColor: "#1890FF",
-            borderRadius: 4,
-            padding: 8,
-            display: "ALWAYS"
-          }
-        },
-        {
-          id: 2,
-          title: "鹿城区政府",
-          latitude: 28.01085,
-          longitude: 120.70321,
-          width: 24,
-          height: 34,
-          callout: {
-            content: "鹿城区政府",
-			color: "#FFFFFF",
-            bgColor: "#52C41A",
-			borderRadius: 4,
-			padding: 8,
-            display: "ALWAYS"
-          }
-        },
-        // 其他标记点...
-        {
-          id: 15,
-          title: "温州南站",
-          latitude: 27.987397,
-          longitude: 120.694927,
-          width: 24,
-          height: 34,
-          callout: {
-            content: "温州南站（高铁站）",
-			color: "#FFFFFF",
-            bgColor: "#FA8C16",
-			borderRadius: 4,
-			padding: 8,
-            display: "ALWAYS"
-          }
-        }
-      ]
+      markers: []
     }
+  },
+  components: {
+  	LoadingComponent
   },
   onLoad()
   {
-	  
+	  this.fetchData()
   },
   methods: {
+	async fetchData()
+	{
+		try {
+			// 调用云函数获取项目详情
+			const res = await uniCloud.callFunction({
+				name: 'db-query',
+				data: {
+					collectionName: 'Project_data',
+					query: {status: 'published'}
+				}
+			});
+			console.log(res.result)
+			if(res.result && res.result.code === 200 && res.result.data && res.result.data)
+			{
+				const projectData = res.result.data.data;
+				for (const project of projectData)
+				{
+					const markersElement = {
+						width: 24,
+						height: 34,
+						callout:
+						{
+							color:"#FFFFFF",
+							bgColor:"#1890FF",
+							borderRadius:4,
+							padding:8,
+							display:"ALWAYS"
+						}
+					};
+					markersElement.id = this.markers.length;
+					markersElement._id = project._id;
+					markersElement.title = project.name;
+					markersElement.callout.content = project.name;
+					const latitude = project.accuratePosition.latitude;
+					const longitude =  project.accuratePosition.longitude;
+					if(latitude && latitude != 0 && latitude >= -90 && latitude <= 90)
+						markersElement.latitude = project.accuratePosition.latitude;
+					else console.error('数据库纬度数据加载异常:', err);
+					if(longitude && longitude != 0 && longitude >= -180 && longitude <= 180)
+						markersElement.longitude = project.accuratePosition.longitude;
+					else console.error('数据库经度数据加载异常:', err);
+					this.markers.push(markersElement)
+				}
+			}
+			else
+			{
+				console.error('数据库查询失败:', err);
+			}
+			this.isLoading = false;
+		} catch (err) {
+			console.error('加载项目详情失败:', err);
+			uni.showToast({
+				title: '加载失败',
+				icon: 'none'
+			});
+			this.isLoading = false;
+		}
+	},
     // 标记点点击事件
     handleMarkerTap(e) {
 	  const targetMarker = this.markers.find(
@@ -95,8 +113,8 @@ export default {
 	  	  success: (res) => {
 	  	    if (res.confirm) {
 	  	      uni.navigateTo({
-	  	      	url:"/pages/Fasttest/Fasttest"
-	  	      })
+	  	      	url: `/pages/Project/ProjectDetail?id=${targetMarker._id}`
+	  	      });
 	  	    } else if (res.cancel) {
 	  	      return;
 	  	    }
